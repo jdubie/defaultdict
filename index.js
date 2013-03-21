ac99d00bbf20ab5f8916830c987840382e354d4a
Jack Dubie <jack@orchestra.com>
2013-03-21 12:54:07 -0700

    Added source file

---
+++ index.js
@@ -0,0 +1,39 @@
+/*!
+ * set
+ * Jack Dubie
+ */
+
+/**
+ * Expose `DefaultDict`.
+ */
+
+exports = module.exports = function DefaultDict(defaultValue) {
+  this.defaultValue = defaultValue;
+  this._ = {};
+}
+
+/**
+ * Set `key` to `value`
+ *
+ * @param {*} key
+ * @param {*} value
+ * @api public
+ */
+
+DefaultDict.prototype.set = function(key, value) {
+  this._[key] = value;
+}
+
+/**
+ * Get `value` for `key` if defined. Otherwise return default
+ *
+ * @param {*} key
+ * @return {*}
+ * @api public
+ */
+
+DefaultDict.prototype.get = function(key, value) {
+  var result = this._[key];
+  if (result) return result;
+  return this.defaultValue;
+}
